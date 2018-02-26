82beaba6b764d4208f01bc2a9c2e6296ffc23e9d
alexander sydorenko <dangerzone@alexanders-MacBook-Pro.local>
2018-02-25 22:13:45 -0700

    add router to Landing page and Library

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Library from './components/Library';
+import Landing from './components/Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <div class = "App">
+        <header>
+          <h1>Bloc Jams</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+        </main>
       </div>
     );
   }
---
+++ src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing = () => (
+	<section className="landing">
+		Landing page will go here. 
+	</section>
+
+	);
+
+export default Landing;
