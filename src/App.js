21e13e5e43e4848987a5dd27f9eb4a652509ebc4
alexander sydorenko <dangerzone@alexanders-MacBook-Pro.local>
2018-02-26 19:28:20 -0700

    add Album component/route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Library from './components/Library';
 import Landing from './components/Landing';
+import Album from './components/Album'
 
 class App extends Component {
   render() {
