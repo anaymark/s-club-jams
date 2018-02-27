da56f768ea44c694b410133d47184fdd08c1bcdd
alexander sydorenko <dangerzone@alexanders-MacBook-Pro.local>
2018-02-27 16:53:50 -0700

    add navigation links

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ class App extends Component {
     return (
         <div class = "App">
         <header>
+          <nav>
+            <Link to="/">Landing</Link>
+            <Link to="/library">Library</Link>
+          </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
