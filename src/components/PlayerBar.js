62d7f31af0e0ca153be343e49e24ec45341ceafb
alexander sydorenko <dangerzone@alexanders-MacBook-Pro.local>
2018-03-01 14:48:45 -0700

    Add universal button for play, previous/next,current-time/total-time and volume sliders

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class PlayerBar extends Component {
+	render() {
+		return(
+		<section className = "player-bar">
+		  <section id="buttons">
+           <button id="previous">
+             <span className="ion-skip-backward" onClick={this.props.handlePrevClick}></span>
+           </button>
+           <button id="play-pause" onClick={this.props.handleSongClick}>
+             <span className="ion-play"></span>
+             <span className="ion-pause"></span>
+             <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
+           </button>
+           <button id="next">
+             <span className="ion-skip-forward"></span>
+           </button>
+         </section>
+         <section id="time-control">                                                                    
+           <div className="current-time">–:––</div>
+           <input type="range" className="seek-bar" value="0" />
+           <div className="total-time">–:––</div>
+         </section>
+         <section id="volume-control">
+           <div className="icon ion-volume-low"></div>
+           <input type="range" className="seek-bar" value="80" />
+           <div className="icon ion-volume-high"></div>
+         </section>
+	   </section>
+		);
+	}
+}
+
+export default PlayerBar;
